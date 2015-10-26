f787b911849947721abd4b44df2d86ead53f84d6
JiangLi <JL_raining@163.com>
2015-10-26 13:34:04 +0800

    静态数字效果

--- draw.js
+++ draw.js
@@ -3,6 +3,9 @@
  */
 var WINDOW_WIDTH = 1024;
 var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
 
 window.onload = function(){
     var canvas = document.getElementById("canvas");
@@ -14,8 +17,32 @@ window.onload = function(){
     render(context);
 }
 function render(cxt){
-    var hour = 12;
-    var minute = 34;
-    var second = 56;
-    
+    var hours = 12;
+    var minutes = 34;
+    var seconds = 56;
+
+    renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
+    renderDigit( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(hours%10) , cxt )
+    renderDigit( MARGIN_LEFT + 30*(RADIUS + 1) , MARGIN_TOP , 10 , cxt )
+    renderDigit( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(minutes/10) , cxt);
+    renderDigit( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(minutes%10) , cxt);
+    renderDigit( MARGIN_LEFT + 69*(RADIUS+1) , MARGIN_TOP , 10 , cxt);
+    renderDigit( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(seconds/10) , cxt);
+    renderDigit( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(seconds%10) , cxt);
+}
+
+function renderDigit(x , y , num ,cxt){
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for( var i=0;i<digit[num].length;i++ ){
+        for(var j=0;j<digit[num][i].length;j++){
+            if(digit[num][i][j] == 1){
+                cxt.beginPath();
+                cxt.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+i*2*(RADIUS+1)+(RADIUS+1),RADIUS,0,2*Math.PI);
+                cxt.closePath();
+
+                cxt.fill();
+            }
+        }
+    }
 }
